244d80f49a8e1fd04c378305e591dda3f04d844b
ande3577 <ande3577@gmail.com>
2013-07-20 11:22:23 -0500

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_issue_mailer_hook do
+  name 'Redmine Issue Mailer Hook plugin'
+  author 'David S Anderson'
+  description 'Adds a hook for the issue mailer view'
+  version '0.0.1'
+  url 'http://www.github.com/ande3577/redmine_issue_mailer_hook'
+  author_url 'http://www.github.com/ande3577'
+end
